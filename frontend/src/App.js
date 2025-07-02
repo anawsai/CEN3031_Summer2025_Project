bf353bd8b2a0b6352a4cbb404e7302d3b6308da5
anawsai <anapetrela8@gmail.com>
2025-07-02 19:47:07 -0400

    Fix frontend-backend connection with /api/health endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,46 @@
-//import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  //react state for backend data
+  const [statusData, setStatusData] = useState({
+    message: "",
+    status: "",
+    app: "",
+  });
+
+  // useEffect to call flask backend once on load
+  useEffect(() => {
+    fetch("/api/health") //react proxy will redirect to flask
+      .then((res) => res.json())
+      .then((data) => {
+        setStatusData({
+          message: data.message,
+          status: data.status,
+          app: data.app,
+        });
+      })
+      .catch((err) => {
+        console.error("Error contacting backend:", err);
+        setStatusData({
+          message: "Failed to contact backend",
+          status: "error",
+          app: "swampscheduler",
+        });
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello World! </h1>
+        <h1>Backend Status</h1>
+        <p>App: {statusData.app}</p>
+        <p>Status: {statusData.status}</p>
+        <p>Message: {statusData.message}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
